e9e1a4670ef6a667d5d6499b06ec0e1271907ad3
Mujahidul12628 <mujahidul15-12628@diu.edu.bd>
2023-04-05 11:10:14 +0600

    products data loaded

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import { useState } from 'react'
 import './App.css'
 import Header from './assets/components/Header/Header'
+import Shop from './assets/components/Shop/Shop'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
+
       <Header></Header>
+      <Shop></Shop>
 
     </div>
   )
--- src/assets/components/Shop/Shop.jsx
+++ src/assets/components/Shop/Shop.jsx
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Shop.css'
 
 const Shop = () => {
+
+    const [products, setProducts] = useState([]);
+    useEffect(() => {
+        fetch('products.json')
+            .then(res => res.json())
+            .then(data => setProducts(data))
+    }, [])
     return (
-        <div>
+        <div className='shop-container'>
+
+            <div className="product-container">
+                <h1>Product container</h1>
+            </div>
+            <div className="cart-container">
+                <h1>Cart container</h1>
+            </div>
+            {
+                products.map(product => {
+                    return <li>{product.category}
+                    </li>
+                })
+            }
 
         </div>
     );
