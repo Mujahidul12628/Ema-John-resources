7c2f362c548731a69619411253e7bd604bfc62d6
Mujahidul12628 <mujahidul15-12628@diu.edu.bd>
2023-04-05 06:10:04 +0600

    Navbar added

--- src/assets/components/Header/Header.jsx
+++ src/assets/components/Header/Header.jsx
@@ -4,7 +4,16 @@ import logo from '../../images/Logo.svg'
 
 const Header = () => {
     return (
-        <></>
+        <nav className='header-content'>
+            <img src={logo} alt="" />
+            <div className="ancor">
+                <a href="/shop">Shop</a>
+                <a href="/order">Order</a>
+                <a href="/inventory">Inventory</a>
+                <a href="/login">Login</a>
+            </div>
+
+        </nav>
     );
 };
 
