dd560f0dfe6aaed658498d76e74b835d03779eb8
Mujahidul12628 <mujahidul15-12628@diu.edu.bd>
2023-04-05 06:09:14 +0600

    structure

---
+++ src/assets/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Header.css';
+import logo from '../../images/Logo.svg'
+
+const Header = () => {
+    return (
+        <></>
+    );
+};
+
+export default Header;
