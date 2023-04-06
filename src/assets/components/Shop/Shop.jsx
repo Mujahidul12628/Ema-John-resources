cd39cc1d2d6e2a55b7bf6339ac26731aba798f38
Mujahidul12628 <mujahidul15-12628@diu.edu.bd>
2023-04-06 15:17:20 +0600

    set card data in localStorage

--- src/assets/components/Shop/Shop.jsx
+++ src/assets/components/Shop/Shop.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import './Shop.css'
 import Product from '../Product/Product';
 import Cart from '../Cart/Cart';
+import { addToDb, getShoppingCart } from '../../utilities/fakedb';
 
 const Shop = () => {
 
@@ -17,8 +18,35 @@ const Shop = () => {
     const handleAddToCart = (product) => {
         const newCart = [...cart, product]
         setCart(newCart);
-        console.log(newCart)
+        addToDb(product.id)
+        // console.log(newCart)
     }
+
+    useEffect(() => {
+        const storedCart = getShoppingCart();
+        // console.log(storedCart)
+
+        //step 1(for in loop)
+        for (const id in storedCart) {
+
+            const savedCart = [];
+
+
+            //step 2 get product by using id
+            const addedProduct = products.find(product => product.id === id)
+            // console.log(addedProduct)
+
+            if (addedProduct) {
+                //step 3 get quantity of each product
+                const quantity = storedCart[id];
+                addedProduct.quantity = quantity;
+                savedCart.push(addedProduct)
+            }
+
+
+
+        }
+    }, [products])
 
     return (
         <div className='shop-container'>
